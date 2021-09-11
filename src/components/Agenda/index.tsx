25b291e8738980ec54d23969f05eac665b8cb57d
Justin Luo <jusluo@paypal.com>
2021-09-11 16:39:21 -0400

    filter dropdown by calendar

--- src/components/Agenda/index.tsx
+++ src/components/Agenda/index.tsx
@@ -1,16 +1,19 @@
-import React, { ReactElement, useContext, useEffect, useMemo, useState } from 'react'
+import React, {
+  ReactElement,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
 import { DateTime } from 'luxon'
-
 import greeting from 'lib/greeting'
-
 import Calendar from 'src/models/Calendar'
 import Event from 'src/models/Event'
 import AccountContext from 'src/context/accountContext'
-
 import List from './List'
 import EventCell from './EventCell'
-
 import style from './style.scss'
+import runEvery from 'lib/runEvery'
 
 type AgendaItem = {
   calendar: Calendar
@@ -28,23 +31,36 @@ const compareByDateTime = (a: AgendaItem, b: AgendaItem) =>
 
 const Agenda = (): ReactElement => {
   const account = useContext(AccountContext)
-  const [hour, setHour] = useState(DateTime.local().hour)
+  const [hour, setHour] = useState(DateTime.local().second)
+  const [selected, setSelected] = useState('all')
+  // This function is triggered when the select changes
+  const selectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value
+    setSelected(value)
+  }
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setHour(DateTime.local().hour)
-    }, 1000)
-    return () => clearInterval(interval)
-  }, [])
-
+  useEffect(
+    () =>
+      runEvery(1000, () => {
+        setHour(DateTime.local().second)
+      }),
+    [],
+  )
+  const filterByCalendar = useMemo(
+    () => (calendars: Calendar[]) => {
+      if (selected === 'all') return calendars
+      return calendars.filter((item) => selected == item.id)
+    },
+    [selected],
+  )
   const events: AgendaItem[] = useMemo(
     () =>
-      account.calendars
+      filterByCalendar(account.calendars)
         .flatMap((calendar) =>
           calendar.events.map((event) => ({ calendar, event })),
         )
         .sort(compareByDateTime),
-    [account],
+    [account.calendars, filterByCalendar],
   )
 
   const title = useMemo(() => greeting(hour), [hour])
@@ -54,6 +70,14 @@ const Agenda = (): ReactElement => {
       <div className={style.container}>
         <div className={style.header}>
           <span className={style.title}>{title}</span>
+          <div className={style.container}>
+            <select onChange={selectChange} className={style.select}>
+              <option key="all">all</option>
+              {account.calendars.map((item: Calendar) => {
+                return <option key={item.id}>{item.id}</option>
+              })}
+            </select>
+          </div>
         </div>
 
         <List>
